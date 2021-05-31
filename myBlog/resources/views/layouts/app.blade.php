bf1a74eea005739a69fb250d5841f940ed3bdd69
Mourad EL Jayi <mouradeljayi98@gmail.com>
2021-05-31 10:55:41 +0100

    add posts view

--- myBlog/resources/views/layouts/app.blade.php
+++ myBlog/resources/views/layouts/app.blade.php
@@ -30,7 +30,7 @@
               </button>
             </div>
             <nav :class="{'flex': open, 'hidden': !open}" class="flex-col flex-grow pb-4 md:pb-0 hidden md:flex md:justify-end md:flex-row">
-              <a class="px-4 py-2 mt-2 text-sm font-semibold text-gray-900 bg-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">Blog</a>
+              <a class="px-4 py-2 mt-2 text-sm font-semibold text-gray-900 bg-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="{{ route('posts.index') }}">Blog</a>
               <a class="px-4 py-2 mt-2 text-sm font-semibold bg-transparent md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">About</a>
               <a class="px-4 py-2 mt-2 text-sm font-semibold bg-transparent md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">Contact</a>
               <div @click.away="open = false" class="relative" x-data="{ open: false }">
---
+++ myBlog/resources/views/posts/show.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+
+<main class="container mx-auto px-6 mt-20">
+  <h2 class="text-left text-white text-2xl mt-4">Single posts</h2>
+  <div class="grid grid-cols-1 gap-4 md:grid-cols-3 mt-6">
+    <a href="#">
+      <div class="bg-white rounded p-4">
+        <img src="{{ asset('images/laravel_image.png') }}">
+        <h4 class="font-bold mt-2">Introduction to PHP Laravel Framework</h4>
+        <h6 class="text-sm"><i class="fas fa-user-edit"></i> By Mourad EL Jayi</h6>
+        <small class="text-sm"><i class="fas fa-clock"></i> 10 Days ago</small>
+      </div>
+    </a>
+  </div>
+</main>
+
+@endsection
